e6cd5d20622fb93d00f1aea696b545326befe784
apalsnerg <birk.hoijer@gmail.com>
2024-04-10 02:39:24 +0200

    Added routes and templates for the deck, draw, and shuffle tasks.

--- src/Cards/Cards.php
+++ src/Cards/Cards.php
@@ -12,7 +12,7 @@ class Card
     
     public function __construct($value=null, $suit=null)
     {
-        $values = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        $values = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
         if (is_null($value)) {
             $this->value = $values[random_int(0, 12)];
         } else {
@@ -30,7 +30,7 @@ class Card
     
 /**
  * Class to practice inheritance.
-    */
+*/
 class GraphicCard extends Card {
 
 public function __construct()
@@ -81,7 +81,7 @@ class DeckOfCards
     public function __construct()
     {
         $suits = ["♣️", "♦️", "♥️", "♠️"];
-        $values = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        $values = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
 
         for ($a = 0; $a < 4; $a++) {
             for ($i = 0; $i < 13; $i++) {
@@ -109,13 +109,58 @@ class DeckOfCards
         shuffle($this->deck);
     }
 
+
+    /**
+     * Draws an amount of cards from the deck and removes it.
+     */
     public function draw($number = 1)
     {
         $returns = [];
-        for ($i = 0; $i < $number; $i++) {
-            $a = $this->deck[$i];
-            $returns[] = $a;
+        for ($i = 0; $i < $number && $i < 53; $i++) {
+            $max_idx = count($this->deck);
+            if ($max_idx > 1) {
+                $random_card_idx = random_int(0, $max_idx - 1);
+                $returns[] = $this->deck[$random_card_idx];
+                unset($this->deck[$random_card_idx]);
+                $this->deck = array_values($this->deck);
+            } elseif ($max_idx == 1) {
+                $returns[] = $this->deck[0];
+                unset($this->deck[0]);
+                $this->deck = array_values($this->deck);
+            }
         }
+        
         return $returns;
+    }
+
+    /*
+     * Draws an amount of cards from the deck and removes it.
+     
+    public function draw($number = 1)
+    {
+        $returns = [];
+        for ($i = 0; $i < $number && $i <= count($this->deck); $i++) {
+            $max_idx = count($this->deck);
+            echo $max_idx;
+            if ($max_idx == 1) {
+                $random_card_idx = 0;
+            } else {
+                $random_card_idx = random_int(0, $max_idx - 1);
+            }
+            $returns[] = $this->deck[$random_card_idx];
+            $newDeck = $this->deck;
+            unset($newDeck[$random_card_idx]);
+            $newDeck = array_values($newDeck);
+            echo count($newDeck);
+            $this->deck = $newDeck;
+        }
+        echo count($this->deck);
+        
+        return $returns;
+    }
+    */
+
+    public function getLength() {
+        return count($this->deck);
     }
 }
--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use App\Cards\Card;
+use App\Cards\DeckOfCards;
+use App\Cards\GraphicCard;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
@@ -35,7 +37,7 @@ class ReportController extends AbstractController
     }
 
     #[Route("/lucky", name:"lucky")]
-    public function lucky(): Response
+    public function lucky(Request $request): Response
     {
 
         $number = random_int(0, 100);
@@ -46,11 +48,12 @@ class ReportController extends AbstractController
             "randImg" => $randImg
         ];
 
+
         return $this->render("lucky.html.twig", $data);
     }
 
     #[Route("/api", name: "api")]
-    public function api(Request $request, RouterInterface $router)
+    public function api(Request $request, RouterInterface $router): Response
     {
         $routes = $router->getRouteCollection();
         $data = [
@@ -91,7 +94,7 @@ class ReportController extends AbstractController
     }
 
     #[Route("/session", name:"session")]
-    public function sesh()
+    public function sesh(): Response
     {
         if (session_status() != "PHP_SESSION_ACTIVE") {
             $session = new Session();
@@ -109,15 +112,28 @@ class ReportController extends AbstractController
         return $this->render("session.html.twig", $data);
     }
 
+    #[Route("/destroy", name:"destroy")]
+    public function seshdestroy(Request $request): Response {
+
+        $session = $request->getSession();
+        $session->clear();
+
+        return $this->redirect("session");
+    }
+
 
     #[Route("/card", name:"card")]
-    public function card() {
+    public function card(): Response {
         if (session_status() != "PHP_SESSION_ACTIVE") {
             $session = new Session();
             $session->start();
         }
+        if (!$session->get("deck")) {
+            $deck = new DeckOfCards();
+            $session->set("deck", $deck);
+        }
 
-        $card = new Card();
+        $card = new GraphicCard();
 
         $data = [
             "card" => $card
@@ -126,4 +142,148 @@ class ReportController extends AbstractController
         return $this->render("card.html.twig", $data);
     }
 
+    #[Route("/card/deck", name:"deck")]
+    public function deck(): Response {
+        if (session_status() != "PHP_SESSION_ACTIVE") {
+            $session = new Session();
+            $session->start();
+        }
+        if (!$session->get("deck")) {
+            $deck = new DeckOfCards();
+            $session->set("deck", $deck);
+        }
+
+        $deckBag = $session->getBag("attributes");
+        $deck = $deckBag->get("deck");
+
+        $data = [
+            "session" => $session,
+            "deck" => $deck
+        ];
+
+        return $this->render("deck.html.twig", $data);
+    }
+
+    #[Route("/card/deck/shuffle", name:"shuffle")]
+    public function shuffle(): Response {
+        if (session_status() != "PHP_SESSION_ACTIVE") {
+            $session = new Session();
+            $session->start();
+        }
+        if (!$session->get("deck")) {
+            $deck = new DeckOfCards();
+            $deck->shuffle();
+            $session->set("deck", $deck);
+        }
+
+        $deckBag = $session->getBag("attributes");
+        $deck = $deckBag->get("deck");
+
+        if ($deck->getLength() == 0) {
+            $deck = new DeckOfCards;
+            $deck->shuffle();
+            $session->set("deck", $deck);
+        }
+        
+        $deck->shuffle();
+
+        $data = [
+            "session" => $session,
+            "deck" => $deck
+        ];
+
+        return $this->render("shuffle.html.twig", $data);
+    }
+
+    #[Route("card/deck/shuffle/reset")]
+    public function reset_shuffle(): Response {
+        if (session_status() != "PHP_SESSION_ACTIVE") {
+            $session = new Session();
+            $session->start();
+        }
+
+        $deck = new DeckOfCards();
+        $deck->shuffle();
+        $session->set("deck", $deck);
+
+        $data = [
+            "session" => $session,
+            "deck" => $deck
+        ];
+
+        return $this->redirect("../shuffle");
+    }
+
+    #[Route("card/deck/newDeckRedirect", name:"newDeckRedirect", methods:["POST"])]
+    public function newDeckRedirect(Request $request): Response {
+        $checkbox = $request->request->get("shfl");
+        if ($checkbox) {
+            $deck = new DeckOfCards;
+            $session = $request->getSession();
+            $session->set("deck", $deck);
+            return $this->redirect("shuffle/reset");
+        }
+        else {
+            $deck = new DeckOfCards;
+            $session = $request->getSession();
+            $session->set("deck", $deck);
+            return $this->redirect("../deck");
+        }
+    }
+
+    #[Route("card/deck/draw", name:"draw")]
+    public function draw(Request $request): Response {
+
+        if (session_status() != "PHP_SESSION_ACTIVE") {
+            $session = new Session();
+            $session->start();
+        }
+        if (!$session->get("deck")) {
+            $deck = new DeckOfCards();
+            $session->set("deck", $deck);
+        }
+
+        $session = $request->getSession();
+        $deck = $session->get("deck");
+        $cards = $deck->draw(1);
+        $session->set("deck", $deck);
+        $length = $deck->getLength();
+
+        $data = [
+            "cards" => $cards,
+            "length" => $length,
+            "deck" => $deck
+        ];
+
+        return $this->render("draw.html.twig", $data);
+    }
+    
+    #[Route("card/deck/draw/{number}", name:"drawnum")]
+    public function drawnum(Request $request, int $number=1): Response {
+
+        if (session_status() != "PHP_SESSION_ACTIVE") {
+            $session = new Session();
+            $session->start();
+        }
+        if (!$session->get("deck")) {
+            $deck = new DeckOfCards();
+            $session->set("deck", $deck);
+        }
+
+        $session = $request->getSession();
+        $deck = $session->get("deck");
+
+        $cards = $deck->draw($number);
+        $session->set("deck", $deck);
+
+        $cardCount = $session->get("deck")->getLength();
+
+        $data = [
+            "cards" => $cards,
+            "length" => $cardCount,
+            "deck" => $deck
+        ];
+
+        return $this->render("draw.html.twig", $data);
+    }
 }
